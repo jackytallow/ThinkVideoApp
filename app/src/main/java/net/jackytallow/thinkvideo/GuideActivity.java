d8cb7fb55599259512cfaf267e8387fb7431653a
jackytallow <3434481891@qq.com>
2020-04-13 16:46:08 +0800

    启动页开发

---
+++ app/src/main/java/net/jackytallow/thinkvideo/GuideActivity.java
@@ -0,0 +1,17 @@
+package net.jackytallow.thinkvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 引导页面
+ */
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
---
+++ app/src/main/java/net/jackytallow/thinkvideo/HomeActivity.java
@@ -0,0 +1,17 @@
+package net.jackytallow.thinkvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 主页
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
