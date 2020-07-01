c1d803bfca04d5c7d1c5ce2b2115f3096dffbf09
jackytallow <3434481891@qq.com>
2020-07-01 21:31:27 +0800

    全局初始化Gson，OKhttp

--- app/src/main/java/net/jackytallow/thinkvideo/api/SohuApi.java
+++ app/src/main/java/net/jackytallow/thinkvideo/api/SohuApi.java
@@ -9,8 +9,56 @@ import net.jackytallow.thinkvideo.model.Channel;
  */
 public class SohuApi extends BaseSiteApi {
 
-    @Override
-    public void onGetChannelAlbums(Channel channel , int pageNo, int pageSize, OnGetChannelAlbumListener listener) {
+    private static final int SOHU_CHANNELID_MOVIE = 1; //搜狐电影频道ID
+    private static final int SOHU_CHANNELID_SERIES = 2; //搜狐电视剧频道ID
+    private static final int SOHU_CHANNELID_VARIETY = 7; //搜狐综艺频道ID
+    private static final int SOHU_CHANNELID_DOCUMENTRY = 8; //搜狐纪录片频道ID
+    private static final int SOHU_CHANNELID_COMIC = 16; //搜狐动漫频道ID
+    private static final int SOHU_CHANNELID_MUSIC = 24; //搜狐音乐频道ID
 
+
+    private final static String API_CHANNEL_ALBUM_FORMAT = "http://api.tv.sohu.com/v4/search/channel.json" +
+            "?cid=%s&o=1&plat=6&poid=1&api_key=9854b2afa779e1a6bff1962447a09dbd&" +
+            "sver=6.2.0&sysver=4.4.2&partner=47&page=%s&page_size=%s";
+
+
+    @Override
+    public void onGetChannelAlbums(Channel channel, int pageNo, int pageSize, OnGetChannelAlbumListener listener) {
+        String url = getChannelAlbumUrl(channel, pageNo, pageSize);
+        doGetChannelAlbumsByUrl(url, listener);
+    }
+
+    private void doGetChannelAlbumsByUrl(String url, OnGetChannelAlbumListener listener) {
+        //TODO 网络请求
+    }
+
+    private String getChannelAlbumUrl(Channel channel, int pageNo, int pageSize) {
+        //格式化url
+        return String.format(API_CHANNEL_ALBUM_FORMAT, toConvertChanneId(channel), pageNo, pageSize);
+    }
+
+    private int toConvertChanneId(Channel channel) {
+        int channelId = -1; //-1 无效值
+        switch (channel.getChannelId()) {
+            case Channel.SHOW:
+                channelId = SOHU_CHANNELID_SERIES;
+                break;
+            case Channel.MOVIE:
+                channelId = SOHU_CHANNELID_MOVIE;
+                break;
+            case Channel.COMIC:
+                channelId = SOHU_CHANNELID_COMIC;
+                break;
+            case Channel.MUSIC:
+                channelId = SOHU_CHANNELID_MUSIC;
+                break;
+            case Channel.DOCUMENTRY:
+                channelId = SOHU_CHANNELID_DOCUMENTRY;
+                break;
+            case Channel.VARIETY:
+                channelId = SOHU_CHANNELID_VARIETY;
+                break;
+        }
+        return channelId;
     }
 }
